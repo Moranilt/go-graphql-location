65c5febaa22ba77f7b7e2d8cdfac5bb750f9790a
d.berezhnoy <unicornxoxo2@gmail.com>
2021-08-13 13:17:12 +0300

    feat: add login and password to users db

--- db/users.sql
+++ db/users.sql
@@ -1,9 +1,11 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
+  login VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) UNIQUE,
+  email VARCHAR(255) UNIQUE NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  phone VARCHAR(255),
-  email VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -15,5 +17,5 @@ CREATE TABLE users (
 --   FOR EACH ROW
 --   EXECUTE PROCEDURE moddatetime (updated_at);
 
-INSERT INTO users (first_name, last_name, phone, email) VALUES('Name', 'Lastname', '986754673823', 'test@mail.com');
-INSERT INTO users (first_name, last_name, phone, email) VALUES('Test', 'TEst', '9878721632163', 'test2@mail.com');
+INSERT INTO users (first_name, last_name, phone, login, password, email) VALUES('Name', 'Lastname', '986754673823', 'name', '325325326', 'test@mail.com');
+INSERT INTO users (first_name, last_name, phone, login, password, email) VALUES('Test', 'TEst', '9878721632163', 'test', '24124', 'test2@mail.com');
